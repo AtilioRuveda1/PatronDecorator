fe85d64b1b03e2f05f6436430e4a4da306341fa2
Atilio Ruveda <atilio_ruveda@hotmail.com>
2022-05-17 13:01:10 -0300

    quinto test rojo

--- Tests/Decorator.test.ts
+++ Tests/Decorator.test.ts
@@ -3,6 +3,8 @@ import EnemigoFuerte from "../Clases/EnemigoFuerte";
 import { CascoDecorator } from "../Clases/CascoDecorator";
 import { ArmaduraDecorator } from "../Clases/ArmaduraDecorator";
 
+
+//ENEMIGO BASE
 test('1)Cuando_EnemigoBaseRecibeDaño_Deberia_DevolverDiez', () =>  {
     const enemigo = new EnemigoBase;
     expect(enemigo.recibeDaño()).toBe(10);
@@ -25,4 +27,10 @@ test('4)Cuando_EnemigoBaseConArmaduraYCascoRecibeDaño_Deberia_Devolver"5,6"', (
     const enemigoBaseConArmadura = new ArmaduraDecorator(enemigo);
     const enemigoBaseConArmaduraYCasco = new CascoDecorator(enemigoBaseConArmadura);
     expect(enemigoBaseConArmaduraYCasco.recibeDaño()).toBe(7*0.8);
+});
+
+//ENEMIGO FUERTE
+test('5)Cuando_EnemigoFuerteRecibeDaño_Deberia_Devolver5', () =>  {
+    const enemigo = new EnemigoBase;
+    expect(enemigo.recibeDaño()).toBe(5);
 });
