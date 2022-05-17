f0a131778b0adb5b85564d86819905f37803384a
Atilio Ruveda <atilio_ruveda@hotmail.com>
2022-05-17 12:44:33 -0300

    Segundo test rojo

--- Tests/Decorator.test.ts
+++ Tests/Decorator.test.ts
@@ -7,3 +7,8 @@ test('1)Cuando_EnemigoBaseRecibeDaño_Deberia_DevolverDiez', () =>  {
     const enemigo = new EnemigoBase;
     expect(enemigo.recibeDaño()).toBe(10);
 });
+
+test('2)Cuando_EnemigoBaseConCasoRecibeDaño_Deberia_DevolverOcho', () =>  {
+    const enemigo = new EnemigoBase;
+    expect(enemigo.recibeDaño()).toBe(8);
+});
