6c775a9e59ae5e35c50ac16094e5e8c182570e1c
Atilio Ruveda <atilio_ruveda@hotmail.com>
2022-05-17 12:40:13 -0300

    Primer test verde

--- Tests/Decorator.test.ts
+++ Tests/Decorator.test.ts
@@ -4,5 +4,6 @@ import { CascoDecorator } from "../Clases/CascoDecorator";
 import { ArmaduraDecorator } from "../Clases/ArmaduraDecorator";
 
 test('1)Cuando_EnemigoBaseRecibeDaño_Deberia_DevolverDiez', () =>  {
-
+    const enemigo = new EnemigoBase;
+    expect(enemigo.recibeDaño()).toBe(10);
 });
