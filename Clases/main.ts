725313b0347bbc25b45b968554ec451341cd602e
Atilio Ruveda <atilio_ruveda@hotmail.com>
2022-05-17 12:39:27 -0300

    Creacion de la carpeta de tests y declaracion de primer test

--- Clases/main.ts
+++ Clases/main.ts
@@ -1,9 +1,9 @@
-import BaseEnemy from "./EnemigoBase";
+import EnemigoBase from "./EnemigoBase";
 import { CascoDecorator } from "./CascoDecorator"
 import { ArmaduraDecorator } from "./ArmaduraDecorator";
-import DifficultEnemy from "./EnemigoFuerte";
+import EnemigoFuerte from "./EnemigoFuerte";
 
-let enemy = new DifficultEnemy();
+let enemy = new EnemigoFuerte();
 let enemyWithHelmet = new CascoDecorator(enemy);
 let enemyWithHelmetAndArmour = new ArmaduraDecorator(enemyWithHelmet);
 
---
+++ Tests/Decorator.test.ts
@@ -0,0 +1,8 @@
+import EnemigoBase from "../Clases/EnemigoBase";
+import EnemigoFuerte from "../Clases/EnemigoFuerte";
+import { CascoDecorator } from "../Clases/CascoDecorator";
+import { ArmaduraDecorator } from "../Clases/ArmaduraDecorator";
+
+test('1)Cuando_EnemigoBaseRecibeDaño_Deberia_DevolverDiez', () =>  {
+
+});
